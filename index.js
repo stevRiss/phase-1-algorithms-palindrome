8d7c9e0bac6a84aa1b66226a0f98277c4bfebc90
Stephen <stevriss22@gmail.com>
2021-09-27 11:14:18 -0400

    commit tree a5748157d2981b085a256df5e291e9bfeb7ddbe7

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let backwards = word.length - 1;
 
+  //console.log(backwards);
+  // word.split(' ');
+  // word.reverse();
+  // word.join('');
+  // console.log(word)
+
+  for(let i = 0; i < backwards/2; i++){
+    let x = word[i];
+    let y = word[backwards - i]
+
+    if(x != y){
+      return false;
+    }
+    
+  }
+  return true;
+    
+  
+  
+}
+console.log(isPalindrome('robot'));
 /* 
   Add your pseudocode here
+we want to take in a string
+then we want to check the characters of that string from front to back and from back to front
+if same return  true,
+if false return false
 */
 
 /*
   Add written explanation of your solution here
 */
 
-// You can run `node index.js` to view these console logs
+//You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
